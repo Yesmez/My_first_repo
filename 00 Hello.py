c457b49db5a13b81fccc40ba5a4ea4e3d72b0b7d
Yesmez <121048285+Yesmez@users.noreply.github.com>
2022-12-19 11:49:36 -0500

    Add files via upload
    Starting this path

---
+++ 00 Hello.py
@@ -0,0 +1,16 @@
+# Esto es un comentario: Hola Mundo
+print ("Hola Python")
+'''
+Esto tambien
+es un comentario
+en varias lineas
+'''
+
+#Consultar el tipo de datos
+print(type("soy un dato str")) # tipo 'str'
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(True)) #tipo 'bool'
+print(type(3 + 1j)) # tipo 'complex'
+print(type(print( 'mi cadena de texto'))) # tipo 'nonetype'
+
